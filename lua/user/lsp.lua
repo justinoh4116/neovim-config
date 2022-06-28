d96d2f7fe4fe09ab2ebbb8f42188daabb14c3c43
Justin Oh <justinoh4116@gmail.com>
2022-06-27 22:38:16 -0700

    change lsp config

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,3 +1,17 @@
-require 'lspconfig'.intelephense.setup{}
-require 'lspconfig'.html.setup{}
-require 'lspconfig'.tsserver.setup{}
+-- require 'lspconfig'.intelephense.setup{}
+-- require 'lspconfig'.html.setup{}
+-- require 'lspconfig'.tsserver.setup{}
+require("nvim-lsp-installer").setup {
+    automatic_installation = true
+}
+
+local lspconfig = require("lspconfig")
+
+lspconfig.sumneko_lua.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.clangd.setup {}
+lspconfig.emmet_ls.setup {}
+lspconfig.jsonls.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.intelephense.setup {}
+lspconfig.html.setup {}
